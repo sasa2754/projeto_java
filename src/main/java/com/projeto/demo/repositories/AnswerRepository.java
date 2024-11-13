2c8d8c02594d7af0084a1da64cea1edd9650bfac
JuanOneD <Juandiego.jd42@gmail.com>
2024-11-13 09:29:00 -0300

    AAA

--- src/main/java/com/projeto/demo/repositories/AnswerRepository.java
+++ src/main/java/com/projeto/demo/repositories/AnswerRepository.java
@@ -1,15 +1,12 @@
 package com.projeto.demo.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projeto.demo.model.Answer;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long>{
-    @Query("SELECT a FROM Answer a WHERE a.idQuestion = :idQuestion")
-    List<Answer> getAnswerByQuestion(Long idQuestion);
+    // @Query("SELECT a FROM Answer a WHERE a.idQuestion = :idQuestion")
+    // List<Answer> getAnswerByQuestion(Long idQuestion);
 }
