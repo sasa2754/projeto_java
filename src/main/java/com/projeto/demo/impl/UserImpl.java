2b5a0dad61db8d5a543ed67c7311f00ea2d03aaf
ingridcriistine <ingridrochacristine@gmail.com>
2024-11-13 10:03:17 -0300

    user impl

--- src/main/java/com/projeto/demo/impl/UserImpl.java
+++ src/main/java/com/projeto/demo/impl/UserImpl.java
@@ -1,5 +1,55 @@
 package com.projeto.demo.impl;
 
-public class UserImpl {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import com.projeto.demo.repositories.UserRepository;
+import com.projeto.demo.services.UserService;
+import com.projeto.demo.model.User;
+
+public class UserImpl implements UserService {
+
+    @Autowired
+    UserRepository repo;
+
+    @Override
+    public ResponseEntity<Object> register(String name, String email, String EDV, String pass) {
+        var validEmail = repo.findByEmail(email);
+        var validEdv = repo.findByEDV(EDV);
+
+        if(!validEmail.isEmpty() || !validEdv.isEmpty()) {
+            return null;
+        }
+
+        var user = new User();
+        user.setName(name);
+        user.setEDV(EDV);
+        user.setEmail(email);
+        user.setPassword(pass);
+        repo.saveAndFlush(user);
+
+        return new ResponseEntity<Object>("Usuário criado com sucesso!", HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Object> login(String email, String EDV, String pass) {
+        var user = repo.findByEmail(email);
+        
+        if(user.isEmpty()) {
+            user = repo.findByEDV(EDV);
+
+            if(user.isEmpty()) {
+                return null;
+            }
+        }
+
+        //verificação de senha
+        // if() {
+        //     return null;
+        // }
+
+        return new ResponseEntity<Object>("Logado com sucesso!", HttpStatus.OK);
+    }
 }
