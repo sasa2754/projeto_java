456bc84cc04e4e575d168ba2ce843c6bbc233393
ingridcriistine <ingridrochacristine@gmail.com>
2024-11-13 09:23:18 -0300

    creating database

---
+++ src/main/java/com/projeto/demo/DependencyConfiguration.java
@@ -0,0 +1,9 @@
+package com.projeto.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DependencyConfiguration {
+    
+}
